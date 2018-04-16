c34750f8df3f54a9d33e11bac7c3d96fdf97764e
Adrien Abraham <aabraham@askin.fr>
2018-04-16 16:58:13 +0200

    Complete Bits factories and tests

--- chiva/bin.py
+++ chiva/bin.py
@@ -1,4 +1,5 @@
-from typing import List, TypeVar
+import binascii
+from typing import List, TypeVar, Union
 
 
 TBits = List[int]  # Should be 1 or 0 only.
@@ -13,18 +14,54 @@ class Bits(object):
     def __str__(self) -> str:
         return "".join([str(b) for b in self.bools])
 
+    def set_value(self, integer: int, num_bits: Union[int, None] =None):
+        """ Represent this integer value.
+
+        Make this Bits instance represent this integer. To keep prepending 0s,
+        specify num_bits to the amount of bits expected to represent this int.
+        If num_bits is smaller than the required amount of bits to represent
+        integer, this Bits won't represent properly integer but a truncated
+        value, In doubt, leave num_bits to its default None value, which will
+        not impose an amount of prepending 0s nor a size limit.
+        """
+        self.bools = []
+        while True:
+            if num_bits == 0:
+                break
+
+            if integer > 0:
+                self.bools.insert(0, integer & 1)
+                integer = integer >> 1
+                if num_bits is not None:
+                    num_bits -= 1
+            elif num_bits is not None and num_bits > 0:
+                self.bools = [0] * num_bits + self.bools
+                break
+            else:
+                break
+
     @staticmethod
     def from_bytes(data: bytes, byteorder: str ='big') -> 'Bits':
+        """ Convert the integer represented the bytes data to Bits. """
         bits = Bits()
         num_bits = len(data) * 8
         integer = int.from_bytes(data, byteorder)
-        while integer or num_bits:
-            bits.bools.insert(0, integer & 1)
-            integer = integer >> 1
-            num_bits -= 1
+        bits.set_value(integer, num_bits)
         return bits
 
     @staticmethod
     def from_hexstring(data: str) -> 'Bits':
+        """ Convert a str with hex digits to Bits. """
+        if data.lower().startswith('0x'):
+            data = data[2:]
+        data_bytes = binascii.unhexlify(data)
+        return Bits.from_bytes(data_bytes)
+
+    @staticmethod
+    def from_bitstring(data: str) -> 'Bits':
+        """ Convert a str with 0 and 1 to Bits. """
+        if data.startswith('0b'):
+            data = data[2:]
         bits = Bits()
+        bits.bools = [int(bit) for bit in data if bit in ('0', '1')]
         return bits
--- chiva/tests/test_bin.py
+++ chiva/tests/test_bin.py
@@ -3,11 +3,41 @@ import pytest
 from chiva.bin import Bits
 
 
-DEAD_BOOLS = [1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1]
+DEAD_BITS = "1101111010101101"  # 0xDEAD
 
 
 def test_bools():
     assert Bits().bools == []
+
+
+def test_set_value():
+    bits = Bits()
+    bits.set_value(0)
+    assert bits.bools == []
+    bits.set_value(1)
+    assert bits.bools == [1]
+    bits.set_value(10)
+    assert bits.bools == [1, 0, 1, 0]
+    bits.set_value(255)
+    assert bits.bools == [1] * 8
+
+    # Test num_bits param
+    bits.set_value(0, num_bits=0)
+    assert bits.bools == []
+    bits.set_value(1, num_bits=0)
+    assert bits.bools == []
+
+    bits.set_value(0, num_bits=1)
+    assert bits.bools == [0]
+    bits.set_value(1, num_bits=1)
+    assert bits.bools == [1]
+
+    bits.set_value(0xFF, num_bits=4)
+    assert bits.bools == [1, 1, 1, 1]
+    bits.set_value(0xFF, num_bits=8)
+    assert bits.bools == [1, 1, 1, 1, 1, 1, 1, 1]
+    bits.set_value(0xFF, num_bits=10)
+    assert bits.bools == [0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
 
 
 def test_str():
@@ -17,20 +47,27 @@ def test_str():
     assert str(bits) == "1"
     bits.bools = [1, 1, 0, 1]
     assert str(bits) == "1101"
-    bits.bools = DEAD_BOOLS
+    bits.bools = DEAD_BITS
     assert str(bits) == "1101111010101101"
 
 
 def test_from_bytes():
-    assert Bits.from_bytes(b"").bools == []
-    assert Bits.from_bytes(b"\x00").bools == [0] * 8
-    assert Bits.from_bytes(b"\xFF").bools == [1] * 8
-    assert Bits.from_bytes(b"\xF0").bools == [1] * 4 + [0] * 4
-    assert Bits.from_bytes(b"\x0F").bools == [0] * 4 + [1] * 4
-    assert Bits.from_bytes(b"\xDE\xAD").bools == DEAD_BOOLS
+    assert str(Bits.from_bytes(b"")) == ""
+    assert str(Bits.from_bytes(b"\x00")) == "00000000"
+    assert str(Bits.from_bytes(b"\xFF")) == "11111111"
+    assert str(Bits.from_bytes(b"\xF0")) == "11110000"
+    assert str(Bits.from_bytes(b"\x0F")) == "00001111"
+    assert str(Bits.from_bytes(b"\xDE\xAD")) == DEAD_BITS
 
 
 def test_from_hexstring():
-    assert Bits.from_hexstring("").bools == []
-    assert Bits.from_hexstring("").bools == []
-    assert Bits.from_hexstring("DEAD").bools == DEAD_BOOLS
+    assert str(Bits.from_hexstring("")) == ""
+    assert str(Bits.from_hexstring("0x00")) == "00000000"
+    assert str(Bits.from_hexstring("DEAD")) == DEAD_BITS
+
+
+def test_from_bitstring():
+    assert str(Bits.from_bitstring("")) == ""
+    assert str(Bits.from_bitstring("1")) == "1"
+    assert str(Bits.from_bitstring("11010100010")) == "11010100010"
+    assert str(Bits.from_bitstring("0b11010100010")) == "11010100010"
